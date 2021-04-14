ed185421502c23e17d797b4ff9e324d3b4f9e334
David <davidlecras@hotmail.com>
2021-04-14 18:38:36 +0200

    starting back end

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -6,9 +6,9 @@ import Rating from '../components/Rating'
 const Product = ({ product }) => {
     return (
         <Card className="my-3 p-3 rounded">
-            <Link to={`/product/${product._id}`}><Card.Img variant="top" src={product.image} /></Link>
+            <Link to={`/products/${product._id}`}><Card.Img variant="top" src={product.image} /></Link>
             <Card.Body>
-                <Link to={`/product/${product._id}`}>
+                <Link to={`/products/${product._id}`}>
                     <Card.Title as='div' ><strong>{product.name}</strong></Card.Title>
                 </Link>
                 <Card.Text as='div'>
