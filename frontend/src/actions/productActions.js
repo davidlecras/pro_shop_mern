1a51a940c8958b5adbdcde8b77c24fd2d7bc355f
David <davidlecras@hotmail.com>
2021-04-18 14:09:01 +0200

    Product Details Reducer & Action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstant'
+import { PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from '../constants/productConstant'
 
 export const listProducts = () => async (dispatch) => {
     try {
@@ -14,6 +14,26 @@ export const listProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message
+                ? error.response.message.data
+                : error.message,
+        })
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+        const { data } = await axios.get(`/api/products/${id}`)
+
+        dispatch({
+            type: PRODUCT_DETAILS_SUCCESS,
+            payload: data
+        })
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_DETAILS_FAIL,
             payload: error.response && error.response.data.message
                 ? error.response.message.data
                 : error.message,
--- frontend/src/containers/HomeScreen.js
+++ frontend/src/containers/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
 
 const HomeScreen = () => {
@@ -16,13 +18,17 @@ const HomeScreen = () => {
     return (
         <>
             <h1>Derniers produits:</h1>
-            {loading ? <h1>Loading</h1> : error ? <h3>{error}</h3> : <Row>
-                {products.map(product => (
-                    <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>}
+            {loading ?
+                <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                    : <Row>
+                        {products.map(product => (
+                            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+            }
         </>
     )
 }
